fix error of gradient not being calculated for corners

test_predict.py
@@ -7,7 +7,7 @@ nc = 11
 max_corners = 12
 img_size = 640
 
-model = YOLO('/home/patrick.wissiak/work/runs/train2/weights/best.pt')
+model = YOLO('/home/patrick.wissiak/work/runs/train21/weights/last.pt')
 
 colors = [(np.random.randint(0, 255), np.random.randint(0, 255), np.random.randint(0, 255)) for _ in range(12)]
 
@@ -29,8 +29,10 @@ for i in range(nc):
 
         corners = r.corners
         for corner in corners.cpu().numpy().astype(np.int32):
-            for c in corner:
-                cv2.circle(img, (int(c[0]), int(c[1])), 5, (255, 0, 0), -1)
+            for i_c, c in enumerate(corner):
+                cv2.circle(img, (int(c[0]), int(c[1])), 5, (0, 0, 255), -1)
+                y_offset = -10 if i_c % 2 == 0 else 10
+                cv2.putText(img, str(i_c), (int(c[0]), int(c[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
         cv2.imwrite(f"würfelnetz-{i}-res-{r_i}.png", img)
 
     #cv2.imwrite(f"test/würfelnetz-{i}.png", img)

ultralytics/utils/loss.py
@@ -751,7 +751,7 @@ class v8CornersLoss(v8DetectionLoss):
         # 75 = 64 (4*16, channel) + 11 (11 corners)
 
         feats, pred_corners = preds if isinstance(preds[0], list) else preds[1]
-        pred_corners = pred_corners.type(torch.float32).detach()
+        pred_corners = pred_corners.type(torch.float32)
 
         #bbox_loss = v8DetectionLoss.__call__(self, feats, batch)
 